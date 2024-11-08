5f21cc535b6d954b5f9bc8bb05a419e0c9808ed8
Iohana <iohanalinhares1@gmail.com>
2024-11-07 23:03:56 -0300

    Implementação das condições de ajudas.

--- scripts/buying_tips.gd
+++ scripts/buying_tips.gd
@@ -18,6 +18,7 @@ func _ready() -> void:
 	var condition = "id = '" + str(user_id) + "'"
 	var user_tips = database.select_rows("tips", condition, ["id"])
 	
+	# VERIFICA SE O USUÁRIO EXISTE NA TABLE OU SE É PRECISO CRIAR UM ITEM DELE
 	if user_tips:
 		print('usuário existe')
 	else:
@@ -25,7 +26,7 @@ func _ready() -> void:
 		user_tips = database.query_with_bindings(insert_table, values)
 		print('usuário adicionado')
 	
-	
+	# VERIFICA SE O USUÁRIO TEM PONTOS, SE NÃO TIVER SETA COMO 0
 	var query_result = database.select_rows("users", condition, ["money"])
 	var money = $"Money"
 	if query_result:
@@ -33,7 +34,36 @@ func _ready() -> void:
 	else:
 		money.text = "0"
 	pass
-
+	
+	if int(money.text) < 80:
+		$ColorRect2/Button.disabled = true
+	else:
+		$ColorRect2/Button.disabled = false
+		
+	if int(money.text) < 40:
+		$ColorRect3/Button.disabled = true
+	else:
+		$ColorRect3/Button.disabled = false
+		
+	if int(money.text) < 20:
+		$ColorRect4/Button.disabled = true
+	else:
+		$ColorRect4/Button.disabled = false
+		
+	if int(money.text) < 120:
+		$ColorRect5/Button.disabled = true
+	else:
+		$ColorRect5/Button.disabled = false
+		
+	if int(money.text) < 220:
+		$ColorRect6/Button.disabled = true
+	else:
+		$ColorRect6/Button.disabled = false
+		
+	if int(money.text) < 100:
+		$ColorRect7/Button.disabled = true
+	else:
+		$ColorRect7/Button.disabled = false
 
 func _process(delta: float) -> void:
 	pass
@@ -44,18 +74,50 @@ func _on_back_pressed() -> void:
 	pass
 
 
-var dynamic_scene = preload("res://levels/modal.tscn") # Caminho para sua cena
+var dynamic_scene = preload("res://levels/modal.tscn")
 var current_scene = null
 
-func load_dynamic_scene(title: String):
+# CARREGA A CENA DE COMPRA DE NOVAS DICAS
+func load_dynamic_scene(title: String, money: int, typeButtonPressed: String):
 	if current_scene != null:
 		current_scene.queue_free()
 	
 	current_scene = dynamic_scene.instantiate()
+	$Node2D.add_child(current_scene)
+	await get_tree().create_timer(0).timeout
+	
 	current_scene.set_title(title)
-	$SceneContainer.add_child(current_scene)
+	current_scene.set_money(money)
+	current_scene.set_type(typeButtonPressed)
 	
-	
+# ENVIO DE INFORMAÇÕES PARA O MODAL DE CONFIRMAÇÃO DE COMPRA
 func _on_button_ai_pressed() -> void:
-	load_dynamic_scene("Título 1")
+	load_dynamic_scene("1 resposta de IA", 80, "ai")
 	pass
+
+
+func _on_button_tip_pressed() -> void:
+	load_dynamic_scene("1 dica", 40, "tip")
+	pass
+
+
+func _on_button_cards_pressed() -> void:
+	load_dynamic_scene("1 dica eliminando opções", 20, "cards")
+	pass
+
+
+func _on_button_combo_pressed() -> void:
+	load_dynamic_scene("1 combo com todas as ajudas", 120, "combo")
+	pass
+
+
+func _on_button_ai_3x_pressed() -> void:
+	load_dynamic_scene("3 respostas de IA", 220, "combo_ai")
+	pass
+
+
+func _on_button_tips_3x_pressed() -> void:
+	load_dynamic_scene("3 dicas", 100, "combo_tips")
+	pass
+
+# FIM DO ENVIO DE INFORMAÇÕES PARA O MODAL DE CONFIRMAÇÃO DE COMPRA
--- scripts/modal.gd
+++ scripts/modal.gd
@@ -1,9 +1,83 @@
 extends Node2D
 
+var database = SQLite
+
 @onready var label: Label = $Label
+@onready var money_label: Label = $Money
+@onready var cancel_button: Button = $Cancel
+
+var tips_result
+var condition
+var typeButtonPressed
+
+func _ready():
+	database = SQLite.new()
+	database.path = "res://database/database.db"
+	database.open_db()
+	
+	#var user_id = Global.user_id
+	var user_id = 11
+	
+	condition = "id = '" + str(user_id) + "'"
+	
+	tips_result = database.select_rows("tips", condition, ["ai, tip, cards"])
+	
+	cancel_button.pressed.connect(_on_cancel_pressed)
 
 func set_title(title_text: String):
 	if label:
 		label.text = title_text
 	else:
-		push_error("TitleLabel não encontrado!")
+		push_error("title_text não encontrado!")
+
+func set_money(money: int):
+	if label:
+		money_label.text = "$" + str(money)
+	else:
+		push_error("money_label não encontrado!")
+
+
+func _on_cancel_pressed() -> void:
+	queue_free()
+	pass
+	
+func set_type(type: String):
+	typeButtonPressed = type
+
+func _on_confirm_pressed():
+	var update_result
+	match typeButtonPressed:
+		'ai':
+			var ai = tips_result[0].ai
+			var update_data = {"ai": ai + 1}
+			update_result = database.update_rows("tips", condition, update_data)
+		'tip':
+			var tip = tips_result[0].tip
+			var update_data = {"tip": tip + 1}
+			update_result = database.update_rows("tips", condition, update_data)
+		'cards':
+			var cards = tips_result[0].cards
+			var update_data = {"cards": cards + 1}
+			update_result = database.update_rows("tips", condition, update_data)
+		'combo':
+			var ai = tips_result[0].ai
+			var tip = tips_result[0].tip
+			var cards = tips_result[0].cards
+			var update_data = {
+				"ai": ai + 1,
+				"tip": tip + 1,
+				"cards": cards + 1
+			}
+			update_result = database.update_rows("tips", condition, update_data)
+		'combo_ai':
+			var ai = tips_result[0].ai
+			var update_data = {"ai": ai + 3}
+			update_result = database.update_rows("tips", condition, update_data)
+		'combo_tips':
+			var tip = tips_result[0].tip
+			var update_data = {"tip": tip + 3}
+			update_result = database.update_rows("tips", condition, update_data)
+	
+	if update_result:
+		queue_free()
+	pass
